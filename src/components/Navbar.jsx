cda0b014b4649596a9224f3f0181b5a45b6fb742
loverboy <loverboykosu@gmail.com>
2025-04-10 22:40:58 +0900

    feat: add hero icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,21 @@
 import { useSelector } from "react-redux";
+import { CartIcon, PlusIcon, MinusIcon } from "../HeroIcons";
 const Navbar = () => {
   const amount = useSelector((store) => store.cart.amount);
   return (
     <nav>
       <div className="nav-center">Navbar</div>
       <h3>Redux Shopping</h3>
-      <div className="nav-center">ロゴ</div>
+      <div className="nav-container">
+        <CartIcon />
+        <PlusIcon />
+        <MinusIcon />
+      </div>
       <div className="amount-container">
         <p className="total-amount">{amount}</p>
       </div>
     </nav>
   );
 };
+
 export default Navbar;
