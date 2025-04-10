4c7b4c28eeb3a5179b804e18026a93fe98fd54e3
loverboy <loverboykosu@gmail.com>
2025-04-10 22:29:18 +0900

    feat: delete app.css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,10 @@ const Navbar = () => {
     <nav>
       <div className="nav-center">Navbar</div>
       <h3>Redux Shopping</h3>
+      <div className="nav-center">ロゴ</div>
+      <div className="amount-container">
+        <p className="total-amount">0</p>
+      </div>
     </nav>
   );
 };
