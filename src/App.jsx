515b80a183c7e5315c29bd668c88e750aadce24c
loverboy <loverboykosu@gmail.com>
2025-04-12 21:50:16 +0900

    feat: delete row

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
 function App() {
   const [count, setCount] = useState(0);
-
   return (
     <>
       <Navbar />
