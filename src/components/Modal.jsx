2a12f4b0bd803ded0f60e0282bb50679f0a0826b
loverboy <loverboykosu@gmail.com>
2025-04-20 23:47:46 +0900

    feat: #40 create 全削除event

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,12 +1,29 @@
+import { useDispatch } from "react-redux";
+import { closeModal } from "../features/modal/ModalSlice";
+import { clearCart } from "../features/cart/CartSlice";
 const Modal = () => {
+  const dispatch = useDispatch();
   return (
     <aside className="modal-container">
       Modal
       <div className="modal">
         <h4>買い物かごをすべて空にしますか？</h4>
         <div className="btn-container">
-          <button className="btn confirm-btn">OK</button>
-          <button className="btn clear-btn">キャンセル</button>
+          <button
+            className="btn confirm-btn"
+            onClick={() => {
+              dispatch(clearCart());
+              dispatch(closeModal());
+            }}
+          >
+            OK
+          </button>
+          <button
+            className="btn clear-btn"
+            onClick={() => dispatch(closeModal())}
+          >
+            キャンセル
+          </button>
         </div>
       </div>
     </aside>
