0a2654f651afcccd82603dd5c9b0bf139eb6c745
loverboy <loverboykosu@gmail.com>
2025-04-20 23:35:48 +0900

    feat: #37 prepare modal reducer

--- src/store.jsx
+++ src/store.jsx
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./features/cart/CartSlice.jsx";
+import modalReducer from "./features/modal/ModalSlice.jsx";
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
+    modal: modalReducer,
   },
 });
